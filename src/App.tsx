2a457ac535ba66dd8c9b0d6aede3ac48ec5b5658
C836 <gl.sepiol@gmail.com>
2022-06-27 19:12:05 -0300

    Inicializando ambiente de desenvolvimento

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+import { Button } from './components/Button/Button.styled'
+
+function App() {
+  return (
+    <div className="App">
+      <Button>
+        aweawe
+      </Button>
+    </div>
+  )
+}
+
+export default App
