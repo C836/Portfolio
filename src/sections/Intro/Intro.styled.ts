abcac29d9d67366f9eee587a16ee13027413eca8
C836 <gl.sepiol@gmail.com>
2022-06-28 13:17:37 -0300

    Estrutura final da sessão Intro

--- src/sections/Intro/Intro.styled.ts
+++ src/sections/Intro/Intro.styled.ts
@@ -1,6 +1,80 @@
 import styled from "styled-components";
 
+export const Curriculo = styled.div`
+  position: absolute;
+  top: 30px;
+  right: 30px;
+`;
+
+export const TextField = styled.article`
+  color: white;
+  width: 70%;
+
+  & p {
+    width: 65%;
+
+    &:nth-of-type(2) {
+      margin-bottom: 2em;
+    }
+  }
+`;
+
+export const Name = styled.div`
+  display: ${(props) => (props.hidden ? "none" : "block")};
+`;
+
+export const NameWrapper = styled.div`
+  font: 600 clamp(35px, 7vw, 80px) "Ubuntu Mono";
+  white-space: nowrap;
+
+  & h1 {
+    width: 100%;
+    margin: 0.8em 0;
+    padding: 0;
+    line-height: 0;
+  }
+`;
+
+export const Hand = styled.span`
+  animation-name: wave-animation;
+  animation-duration: 2.5s;
+  animation-iteration-count: infinite;
+  transform-origin: 70% 70%;
+  display: inline-block;
+
+  @keyframes wave-animation {
+    0% {
+      transform: rotate(0deg);
+    }
+    10% {
+      transform: rotate(14deg);
+    }
+    20% {
+      transform: rotate(-8deg);
+    }
+    30% {
+      transform: rotate(14deg);
+    }
+    40% {
+      transform: rotate(-4deg);
+    }
+    50% {
+      transform: rotate(10deg);
+    }
+    60% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(0deg);
+    }
+  }
+`;
+
 export const Intro = styled.section`
   width: 100%;
   height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "Nunito";
 `;
--- src/sections/Intro/Intro.tsx
+++ src/sections/Intro/Intro.tsx
@@ -1,28 +1,62 @@
+import { useState } from "react";
+
 import * as S from "./Intro.styled";
 import { Config } from "./Intro.config";
 
 import Button from "../../components/Button/Button";
 import Dropdown from "../../components/Dropdown/Dropdown";
+import Glitch from "../../components/Glitch/Glitch";
+
+import Typewriter from "typewriter-effect";
 
 import { socials } from "../../data/socials";
 
 export default function Intro(props: Config) {
+  const [nameActive, setActive] = useState(false);
+
   return (
     <S.Intro>
       <S.Curriculo>
-        <Button
-        text="Currículo" />
+        <Button text="Currículo" />
       </S.Curriculo>
-      
+
       <S.TextField>
-        <h2>Olá <S.Hand>👋</S.Hand>, meu nome é</h2>
-        <h1></h1>
+        <h2>
+          Olá <S.Hand>👋</S.Hand>, meu nome é
+        </h2>
+        <S.NameWrapper
+          onMouseEnter={(e) => setActive(true)}
+          onMouseLeave={(e) => setActive(false)}
+        >
+          <S.Name hidden={nameActive ? true : false}>
+            <Typewriter
+              options={{
+                delay: 70,
+              }}
+              onInit={(typewriter) => {
+                typewriter
+                  .pauseFor(500)
+                  .typeString("<" + "Gabriel Lopes")
+                  .typeString(" />")
+                  .start();
+              }}
+            />
+          </S.Name>
+
+          <Glitch hidden={nameActive ? false : true}>
+            {"<Gabriel Lopes />"}
+          </Glitch>
+        </S.NameWrapper>
+
         <h2>Sejam bem vindes ao meu portfólio 😎</h2>
 
-        <p>Sou um desenvolvedor web e designer gráfico. Abaixo você encontra um pouco mais sobre mim e alguns de meus projetos favoritos.</p>
+        <p>
+          Sou um desenvolvedor web e designer gráfico. Abaixo você encontra um
+          pouco mais sobre mim e alguns de meus projetos favoritos.
+        </p>
         <p>Obrigado por estar aqui! 😄</p>
-        
-        <Button text="Contato"/>
+
+        <Button text="Contato" />
       </S.TextField>
 
       <Dropdown icons={socials} />
