aebde615e0d7fe5f7db1654b64cb4263ea0b3279
C836 <gl.sepiol@gmail.com>
2022-06-27 23:56:29 -0300

    FEAT: Implementando lista de contato

---
+++ src/data/socials.tsx
@@ -0,0 +1,20 @@
+import { FaLinkedin, FaGithub, FaEnvelope, FaWhatsapp } from "react-icons/fa"
+
+export const socials = [
+  {
+    icon: <FaLinkedin size={30} />,
+    url: "https://www.linkedin.com/in/gabriel-lopes8/",
+  },
+  {
+    icon: <FaGithub size={30} />,
+    url: "https://github.com/C836",
+  },
+  {
+    icon: <FaEnvelope size={30} />,
+    url: "mailto:gl.sepiol@gmail.com",
+  },
+  {
+    icon: <FaWhatsapp size={30} />,
+    url: "https://wa.me/21984739053/?text=Olá Gabriel!",
+  },
+];
