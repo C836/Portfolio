5cfcc4809179265174677b17bd8306f94cb82c95
C836 <gl.sepiol@gmail.com>
2022-06-28 01:39:02 -0300

    Conteúdo da sessão inicial da página

--- src/sections/Intro/Intro.tsx
+++ src/sections/Intro/Intro.tsx
@@ -1,5 +1,6 @@
 import * as S from "./Intro.styled";
 import { Config } from "./Intro.config";
+
 import Button from "../../components/Button/Button";
 import Dropdown from "../../components/Dropdown/Dropdown";
 
@@ -8,7 +9,22 @@ import { socials } from "../../data/socials";
 export default function Intro(props: Config) {
   return (
     <S.Intro>
-      <Button text="Currículo" />
+      <S.Curriculo>
+        <Button
+        text="Currículo" />
+      </S.Curriculo>
+      
+      <S.TextField>
+        <h2>Olá <S.Hand>👋</S.Hand>, meu nome é</h2>
+        <h1></h1>
+        <h2>Sejam bem vindes ao meu portfólio 😎</h2>
+
+        <p>Sou um desenvolvedor web e designer gráfico. Abaixo você encontra um pouco mais sobre mim e alguns de meus projetos favoritos.</p>
+        <p>Obrigado por estar aqui! 😄</p>
+        
+        <Button text="Contato"/>
+      </S.TextField>
+
       <Dropdown icons={socials} />
     </S.Intro>
   );
