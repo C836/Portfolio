de3b41243195ecad905ad3af8d557d3a9fcf205d
C836 <gl.sepiol@gmail.com>
2022-06-30 21:34:36 -0300

    Template dos dados para a sessão de projetos

---
+++ src/data/projects/highlights.ts
@@ -0,0 +1,13 @@
+export const highlights = [
+    {
+        nome: "Lorem Ipsum",
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget metus vel dui volutpat ornare vitae malesuada diam. Etiam ac lacinia sem, nec convallis nunc. Etiam bibendum vulputate felis, ac semper orci blandit vitae.",
+        tech: ["Place", "Holder", "Place", "Holder", "Place", "Holder"],
+        cover: "https://portfolio-gnfumy51z-c836.vercel.app/assets/images/Projects/mapa.png",
+        gif: "https://c.tenor.com/qMZe3e1Er7EAAAAC/example.gif",
+        links: {
+            web: "https://www.google.com",
+            github: "https://www.github.com"
+        }
+    }
+]
