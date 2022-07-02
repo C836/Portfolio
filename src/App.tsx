04b9a86e3bc08026b7be68f495dd9b8aec0f3860
C836 <gl.sepiol@gmail.com>
2022-07-02 00:37:22 -0300

    Melhorando legibilidade do algorítimo de Scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState, WheelEvent } from "react";
-import { getScrollDirection } from "./utils/ScrollDirection/ScrollDirection";
+import { getScrollDirection } from "./utils/Scroll/ScrollDirection";
 
 import { GlobalStyle } from "./global";
 
@@ -7,10 +7,9 @@ import Intro from "./sections/Intro/Intro";
 import About from "./sections/About/About";
 import Highlights from "./sections/Projects/Highlights/Highlights";
 
-const appSections = [ "Intro", "About" ]
-
 function App() {
-  const [currentPagePosition, setPagePosition] = useState(0);
+  const appSections = [ "Intro", "About" ]
+  const [currentPageIndex, setPageIndex] = useState(0);
 
   const scroll:any = {
     toIntro: () => {
@@ -27,12 +26,14 @@ function App() {
   const handleScroll = (event: WheelEvent) => {
     const direction = getScrollDirection(event);
 
-    setPagePosition(direction === "up" ? 0 : 1);
+    setPageIndex(direction === "up" ? 0 : 1);
   };
 
   useEffect(()=> {
-    scroll[`to${appSections[currentPagePosition]}`]()
-  }, [currentPagePosition])
+    const currentPageSection = appSections[currentPageIndex]
+
+    scroll["to" + currentPageSection]()
+  }, [currentPageIndex])
 
   return (
     <div onWheel={handleScroll}>
