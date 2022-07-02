4e3d648b4625b8a82b4341357f8128651246d41b
C836 <gl.sepiol@gmail.com>
2022-07-01 23:51:23 -0300

    Simplificando Hook para função novamente

--- src/utils/ScrollDirection/ScrollDirection.ts
+++ src/utils/ScrollDirection/ScrollDirection.ts
@@ -1,26 +1,9 @@
-import { useEffect, useState } from "react";
+import { WheelEvent } from "react";
 
-export enum ScrollDirection {
-  up = "up",
-  down = "down",
-}
-
-export const useScrollDirection = () => {
-  const [scrollDir, setScrollDir] = useState(ScrollDirection.up);
-
-  const handleScroll = (event: WheelEvent) => {
-    if (event.deltaY < 0) {
-      setScrollDir(ScrollDirection.up);
-    } else if (event.deltaY > 0) {
-      setScrollDir(ScrollDirection.down);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("wheel", handleScroll);
-
-    return () => window.removeEventListener("wheel", handleScroll);
-  }, []);
-
-  return scrollDir;
+export const getScrollDirection = (event: WheelEvent) => {
+  return (
+    event.deltaY < 0 
+    ? "up"
+    : "down"
+  )
 };
