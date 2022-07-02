3707e60818bfb6bba03ebd08f78bb3fe03c3dce9
C836 <gl.sepiol@gmail.com>
2022-07-01 22:19:22 -0300

    Editando hook ScrollDirection, agora ativando com a interação da roda do mouse

--- src/utils/ScrollDirection/ScrollDirection.ts
+++ src/utils/ScrollDirection/ScrollDirection.ts
@@ -6,41 +6,21 @@ export enum ScrollDirection {
 }
 
 export const useScrollDirection = () => {
-  const threshold = 100;
   const [scrollDir, setScrollDir] = useState(ScrollDirection.up);
 
+  const handleScroll = (event: WheelEvent) => {
+    if (event.deltaY < 0) {
+      setScrollDir(ScrollDirection.up);
+    } else if (event.deltaY > 0) {
+      setScrollDir(ScrollDirection.down);
+    }
+  };
+
   useEffect(() => {
-    let previousScrollYPosition = window.scrollY;
+    window.addEventListener("wheel", handleScroll);
 
-    const scrolledMoreThanThreshold = (currentScrollYPosition: number) =>
-      Math.abs(currentScrollYPosition - previousScrollYPosition) > threshold;
-
-    const isScrollingUp = (currentScrollYPosition: number) =>
-      currentScrollYPosition > previousScrollYPosition &&
-      !(previousScrollYPosition > 0 && currentScrollYPosition === 0) &&
-      !(currentScrollYPosition > 0 && previousScrollYPosition === 0);
-
-    const updateScrollDirection = () => {
-      const currentScrollYPosition = window.scrollY;
-
-      if (scrolledMoreThanThreshold(currentScrollYPosition)) {
-        const newScrollDirection = isScrollingUp(currentScrollYPosition)
-          ? ScrollDirection.down
-          : ScrollDirection.up;
-        setScrollDir(newScrollDirection);
-        previousScrollYPosition =
-          currentScrollYPosition > 0 ? currentScrollYPosition : 0;
-      }
-    };
-
-    const onScroll = () => window.requestAnimationFrame(updateScrollDirection);
-
-    window.addEventListener("scroll", onScroll);
-
-    return () => window.removeEventListener("scroll", onScroll);
+    return () => window.removeEventListener("wheel", handleScroll);
   }, []);
 
   return scrollDir;
 };
-
-// créditos: https://github.com/chicio
