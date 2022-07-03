2d288bc5d4787314ef152fed149d184bc2828306
C836 <gl.sepiol@gmail.com>
2022-07-03 01:00:29 -0300

    Posicionando sessão de projetos em destaque

--- src/sections/Projects/Highlights/Highlights.styled.ts
+++ src/sections/Projects/Highlights/Highlights.styled.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
 
 export const Highlights = styled.div`
+    margin: 30px 0;
 `
--- src/sections/Projects/Highlights/Highlights.tsx
+++ src/sections/Projects/Highlights/Highlights.tsx
@@ -1,18 +1,21 @@
 import * as S from "./Highlights.styled";
 import { Config } from "./Highlights.config";
 import Highlight from "../../../components/Projects/Highlight/Highlight";
+import { Text, Title } from "../../../components/Text/Text.styled";
 
 export default function Highlights(props: Config) {
-  const { HighlightRef1, HighlightRef2, HighlightRef3 } = props;;
+  const { HighlightRef1, HighlightRef2, HighlightRef3 } = props;
 
   return (
-    <section>
-      <h1>Projects</h1>
-      <p>A seguir estão alguns de meus projetos favoritos até o momento.</p>
+    <>
+      <Title>Projects</Title>
+      <Text>A seguir estão alguns de meus projetos favoritos até o momento.</Text>
 
-      <Highlight HighlightRef={HighlightRef1} />
-      <Highlight HighlightRef={HighlightRef2} right />
-      <Highlight HighlightRef={HighlightRef3} />
-    </section>
+      <S.Highlights>
+        <Highlight HighlightRef={HighlightRef1} />
+        <Highlight HighlightRef={HighlightRef2} right />
+        <Highlight HighlightRef={HighlightRef3} />
+      </S.Highlights>
+    </>
   );
 }
