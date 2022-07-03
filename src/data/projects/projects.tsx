5a863235ba4b845a8c4f3339d4014229d026274b
C836 <gl.sepiol@gmail.com>
2022-07-03 01:47:57 -0300

    Template dos dados para a sessão extra de projetos

---
+++ src/data/projects/projects.tsx
@@ -0,0 +1,14 @@
+import { FaReact } from "react-icons/fa"
+
+export const projects = [
+    {
+        nome: "Lorem Ipsum",
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget metus vel dui volutpat ornare vitae malesuada diam. Etiam ac lacinia sem, nec convallis nunc. Etiam bibendum vulputate felis, ac semper orci blandit vitae.",
+        tech: <FaReact/>,
+        cover: "https://portfolio-gnfumy51z-c836.vercel.app/assets/images/Projects/mapa.png",
+        links: {
+            web: "https://www.google.com",
+            github: "https://www.github.com"
+        }
+    }
+]
