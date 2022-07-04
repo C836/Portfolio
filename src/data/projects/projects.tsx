1ed92eb6a3782a147e0940d26129b00d21a524a9
C836 <gl.sepiol@gmail.com>
2022-07-04 18:20:50 -0300

    Atualizando template dos projetos para arrays de objetos

--- src/data/projects/projects.tsx
+++ src/data/projects/projects.tsx
@@ -1,13 +1,62 @@
 import { FaReact } from "react-icons/fa"
 
 export const projects = [
-    {
-        nome: "Lorem Ipsum",
-        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget metus vel dui volutpat ornare vitae malesuada diam. Etiam ac lacinia sem, nec convallis nunc. Etiam bibendum vulputate felis, ac semper orci blandit vitae.",
-        techs: [<FaReact color="#5dd2f3"/>, <FaReact color="#5dd2f3"/>,<FaReact color="#5dd2f3"/>],
-        links: {
-            web: "https://www.google.com",
-            github: "https://www.github.com"
+    [
+        {
+            nome: "Lorem Ipsum",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget metus vel dui volutpat ornare vitae malesuada diam. Etiam ac lacinia sem, nec convallis nunc. Etiam bibendum vulputate felis, ac semper orci blandit vitae.",
+            techs: [<FaReact color="#5dd2f3"/>, <FaReact color="#5dd2f3"/>,<FaReact color="#5dd2f3"/>],
+            links: {
+                web: "https://www.google.com",
+                github: "https://www.github.com"
+            }
+        },
+        {
+            nome: "Lorem Ipsum",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget metus vel dui volutpat ornare vitae malesuada diam. Etiam ac lacinia sem, nec convallis nunc. Etiam bibendum vulputate felis, ac semper orci blandit vitae.",
+            techs: [<FaReact color="#5dd2f3"/>, <FaReact color="#5dd2f3"/>,<FaReact color="#5dd2f3"/>],
+            links: {
+                web: "https://www.google.com",
+                github: "https://www.github.com"
+            }
+        },
+        {
+            nome: "Lorem Ipsum",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget metus vel dui volutpat ornare vitae malesuada diam. Etiam ac lacinia sem, nec convallis nunc. Etiam bibendum vulputate felis, ac semper orci blandit vitae.",
+            techs: [<FaReact color="#5dd2f3"/>, <FaReact color="#5dd2f3"/>,<FaReact color="#5dd2f3"/>],
+            links: {
+                web: "https://www.google.com",
+                github: "https://www.github.com"
+            }
         }
-    }
+    ],
+    [
+        {
+            nome: "Lorem Ipsum",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget metus vel dui volutpat ornare vitae malesuada diam. Etiam ac lacinia sem, nec convallis nunc. Etiam bibendum vulputate felis, ac semper orci blandit vitae.",
+            techs: [<FaReact color="#5dd2f3"/>, <FaReact color="#5dd2f3"/>,<FaReact color="#5dd2f3"/>],
+            links: {
+                web: "https://www.google.com",
+                github: "https://www.github.com"
+            }
+        },
+        {
+            nome: "Lorem Ipsum",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget metus vel dui volutpat ornare vitae malesuada diam. Etiam ac lacinia sem, nec convallis nunc. Etiam bibendum vulputate felis, ac semper orci blandit vitae.",
+            techs: [<FaReact color="#5dd2f3"/>, <FaReact color="#5dd2f3"/>,<FaReact color="#5dd2f3"/>],
+            links: {
+                web: "https://www.google.com",
+                github: "https://www.github.com"
+            }
+        },
+        {
+            nome: "Lorem Ipsum",
+            descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget metus vel dui volutpat ornare vitae malesuada diam. Etiam ac lacinia sem, nec convallis nunc. Etiam bibendum vulputate felis, ac semper orci blandit vitae.",
+            techs: [<FaReact color="#5dd2f3"/>, <FaReact color="#5dd2f3"/>,<FaReact color="#5dd2f3"/>],
+            links: {
+                web: "https://www.google.com",
+                github: "https://www.github.com"
+            }
+        }
+    ]
 ]
