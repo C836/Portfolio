fc5a8eaee911b723badcdcf0bc876c89d5e1bf60
C836 <gl.sepiol@gmail.com>
2022-07-10 17:11:22 -0300

    Ajustando tamanho dos itens do dropdown

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -4,6 +4,7 @@ import * as S from "./Dropdown.styled";
 import { Config } from "./Dropdown.config";
 
 import { FaPlus } from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 export default function Dropdown(props: Config) {
   const { icons } = props;
@@ -22,11 +23,15 @@ export default function Dropdown(props: Config) {
 
       <S.Menu>
         {icons.map((item, index) => (
-          <S.Item active={dropActive}
-          target="_blank"
-          key={index} 
-          href={item.url}>
-            {item.icon} 
+          <S.Item
+            active={dropActive}
+            target="_blank"
+            key={index}
+            href={item.url}
+          >
+            <IconContext.Provider value={{ size: "30" }}>
+              {item.icon}
+            </IconContext.Provider>
           </S.Item>
         ))}
       </S.Menu>
