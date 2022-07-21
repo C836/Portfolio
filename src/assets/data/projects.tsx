4eadbbcf6fdf811f240fc43795a1db1ba312c071
C836 <gl.sepiol@gmail.com>
2022-07-20 23:48:26 -0300

    Atualizando lista de projetos

--- src/assets/data/projects.tsx
+++ src/assets/data/projects.tsx
@@ -1,12 +1,11 @@
 import { FaJs, FaAngular, FaNodeJs } from "react-icons/fa";
+import { SiNestjs } from "react-icons/si";
 
 export const projects = [
   {
     nome: "Calculadora Geográfica",
     descricao:
       "Uma aplicação para calcular a distância entre dois ou mais pontos do mundo apenas pelo seu endereço! O sistema irá resolver a geolocalização e exibir a distância de qualquer lugar do planeta. ",
-    extra:
-      "Além disso, também conta com um painel que exibe o clima e o fuso horário do local selecionado, tudo isso em uma interface simples e intuitiva",
     tech: { icon: <FaJs />, color: "#efd81d" },
     techs: ["Leaflet", "OpenStreetMap", "Nominatim API", "Weather API"],
     cover:
@@ -20,9 +19,7 @@ export const projects = [
   {
     nome: "GreatDex",
     descricao:
-      "Meu primeiro projeto utilizando o framework Angular. A aplicação consiste em uma pokédex interativa utilizando PokeAPI para a requisição de todos os dados dos pokémons. ",
-    extra:
-      "Cada entrada da pokédex contém seu sprite animado e informações de combate, como stats e seus tipos. Atualmente exibindo todos os 913 pokémons!",
+      "Uma pokédex interativa utilizando PokeAPI para a requisição de todos os dados dos pokémons. Cada entrada da pokédex contém seu sprite animado e informações de combate, como stats e seus tipos. Atualmente exibindo todos os 913 pokémons!",
     tech: { icon: <FaAngular />, color: "#d6002f" },
     techs: ["Typescript", "SCSS", "PokeAPI", "Karma", "Jasmine"],
     cover:
@@ -36,9 +33,7 @@ export const projects = [
   {
     nome: "Online Chat",
     descricao:
-      "Uma sala de bate papo simples e dinâmica para conversar com seus amigos! A aplicação conta com um histórico de mensagens, uma lista de usuários ativos e um sistema de mensagens privada. ",
-    extra:
-      "A página contém um sistema de customização, permitindo alternar entre modo escuro ou claro as cores principais do layout.",
+      "Uma sala de bate papo simples e dinâmica para conversar com seus amigos! A aplicação conta com um histórico de mensagens, uma lista de usuários ativos e um sistema de mensagens privada.",
     tech: { icon: <FaNodeJs />, color: "#7fc728" },
     techs: ["HTML", "CSS", "Javascript", "Socket.IO"],
     cover:
@@ -50,19 +45,17 @@ export const projects = [
     },
   },
   {
-    nome: "Online Chat",
+    nome: "Filmes API",
     descricao:
-      "Uma sala de bate papo simples e dinâmica para conversar com seus amigos! A aplicação conta com um histórico de mensagens, uma lista de usuários ativos e um sistema de mensagens privada. ",
-    extra:
-      "A página contém um sistema de customização, permitindo alternar entre modo escuro ou claro as cores principais do layout.",
-    tech: { icon: <FaNodeJs />, color: "#7fc728" },
-    techs: ["HTML", "CSS", "Javascript", "Socket.IO"],
+      "Sistema de registro de filmes seguindo o padrão de interface API Restful. O Projeto conta com uma documentação com todas as rotas, modelos e exemplos das requisições. ",
+    tech: { icon: <SiNestjs />, color: "#d8244c" },
+    techs: ["Typescript", "SQLite", "Jest", "Swagger"],
     cover:
-      "https://raw.githubusercontent.com/C836/Online-chat/main/assets/components/images/chat.png",
+      "https://gcdnb.pbrd.co/images/lzjVFG67GwAm.png?o=1",
     gif: "https://c.tenor.com/qMZe3e1Er7EAAAAC/example.gif",
     links: {
-      web: "https://c836-online-chat.herokuapp.com/",
-      github: "https://github.com/C836/Online-chat",
+      web: "https://filmes-apirest.herokuapp.com/api",
+      github: "https://github.com/C836/Nest-Filmes-API",
     },
-  }
+  },
 ];
