7ae447f7c284c12656d077263ae92f60c107fa10
C836 <gl.sepiol@gmail.com>
2022-07-20 22:42:42 -0300

    Ajustando responsividade da sessão de tecnologias

--- src/sections/Stacks/Stacks.styled.ts
+++ src/sections/Stacks/Stacks.styled.ts
@@ -8,27 +8,54 @@ export const Stacks = styled.section`
   text-align: center;
   width: 100%;
   height: 100vh;
-  margin-bottom: 100px;
-  @media (max-width: 850px){
+  margin-bottom: 80px;
+
+  @media (max-width: 850px) {
     height: auto;
   }
-`
+`;
 
 export const Icons_Wrapper = styled.div`
   margin-top: 20px;
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
-  column-gap: 50px;
-  row-gap: 40px;
+  column-gap: 40px;
+  row-gap: 30px;
+
+  @media (max-width: 850px) {
+    column-gap: 30px;
+    row-gap: 20px;
+  }
+
+  @media (max-width: 450px) {
+    column-gap: 15px;
+    row-gap: 5px;
+  }
 `;
 
 export const Icon = styled.div`
-  font-family: 'Raleway';
-  font-size: .9rem;
+  font-family: "Raleway";
+  font-size: 0.9rem;
+
+  @media (max-width: 850px) {
+    font-size: 0.8rem;
+  }
+
+  @media (max-width: 550px) {
+    font-size: 0.7rem;
+  }
 
   figure {
     margin: 0;
-    font-size: 4rem;
+    font-size: 3.5rem;
+
+    @media (max-width: 850px) {
+      font-size: 2.5rem;
+    }
+
+    @media (max-width: 450px) {
+      font-size: 2rem;
+    }
   }
 `;
